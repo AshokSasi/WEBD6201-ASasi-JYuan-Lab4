7f00cb26d5b8d04be779bcf2f7027d503f89784f
Ashok Sasitharan <asasitharan28@gmail.com>
2021-04-14 19:58:24 -0400

    fixed application not connecting to mongo db atlas

---
+++ .history/Server/Config/db_20210414195410.ts
@@ -0,0 +1,5 @@
+export const LocalURI = "mongodb://localhost/webd6201";
+export const RemoteURI = "mongodb+srv://ashok:niBsnt3bHh9ARjRI@webd6201-lab4.thqp3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+export const Secret = "someSecret";
+export const Host = "MongoDB Atlas";
+
--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Host = exports.Secret = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/webd6201";
-exports.RemoteURI = "mongodb+srv://ashok:branderspassword@webd6201.zr8pg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+exports.RemoteURI = "mongodb+srv://ashok:niBsnt3bHh9ARjRI@webd6201-lab4.thqp3.mongodb.net/webd6201-lab4db?retryWrites=true&w=majority";
 exports.Secret = "someSecret";
 exports.Host = "MongoDB Atlas";
 //# sourceMappingURL=db.js.map
