50d60367d77cc030501203c908efba4c0ed192e8
Ashok Sasitharan <asasitharan28@gmail.com>
2021-04-14 16:17:44 -0400

    fixed login button not being submit

--- .history/public/Scripts/app_20210414161149.ts
+++ .history/public/Scripts/app_20210414161149.ts
@@ -1,8 +1,8 @@
 
-
 namespace core
 {
-   
+  
+
     function testFullName(): void
     {
       let messageArea = $("#messageArea").hide();
@@ -93,45 +93,114 @@ namespace core
           }
 
           location.href = '/contact';
+          
         });
     }
 
     function displayContactList() :void
     {
       // don't allow visitors to go here
-
-
-      // confirm deletion
+      authGuard();
+      //Confirm deletion
       $("a.delete").on("click", function(event){
         if(!confirm("Are you sure?"))
         {
           event.preventDefault();
-          location.href = '/contact-list';
-        }       
+         location.href = '/contact-list';
+        }
+       
       });
 
-      
+
     }
 
-    function displayEdit(): void
+     function displayEdit(): void
     {
+      
       // form validation
       formValidation();
-     } 
+    } 
 
     function displayLogin():void
     {
-      //TODO Validation
+      let messageArea = $("#messageArea");
+      messageArea.hide();
+
+      $("#loginButton").on("click", function() 
+      {
+        let username = $("#username");
+        let password = $("#password");
+        let success = false;
+        let newUser = new core.User();
+
+        // use ajax to access the json file
+        $.get("./Data/users.json", function(data)
+        {
+          // check each user in the users.json file  (linear search)
+          for (const user of data.users) 
+          {
+            if(username.val() == user.Username && password.val() == user.Password)
+            {
+              newUser.fromJSON(user);
+              success = true;
+              break;
+            }
+          }
+
+          // if username and password matches - success... then perform login
+          if(success)
+          {
+            // add user to session storage
+            sessionStorage.setItem("user", newUser.serialize());
+
+            // hide any error message
+            messageArea.removeAttr("class").hide();
+
+            $("form").trigger("submit");
+          }
+          else
+          {
+            // display an error message
+            username.trigger("focus").trigger("select");
+            messageArea.show().addClass("alert alert-danger").text("Error: Invalid login information");
+          }
+        });
+      });
+
+      $("#cancelButton").on("click", function()
+      {
+        // clear the login form
+        document.forms[0].reset();
+        
+        // return to the home page
+        location.href = '/home';
+      });
     }
 
+    function performLogout():void
+    {
+        sessionStorage.clear();
+        location.href = '/login';
+    }
 
-   function displayRegister()
-   {
+    
 
-   }
+    function authGuard():void
+    {
+      if(!sessionStorage.getItem("user"))
+      {
+      // redirect back to login page
+      //loadLink("login");
+      location.href = '/login';
+      }
+    }
+
+    function display404():void
+    {
+
+    }
 
    
-    
 
     /**
      * This is the entry point for our program
@@ -139,20 +208,22 @@ namespace core
      */
     function Start(): void
     {
+     
         let pageID = $("body")[0].getAttribute("id");
         
       switch (pageID) {
-        case 'edit':
-          displayEdit();
-          break;
+       case 'edit':
+         displayEdit();
+         break;
         case 'contact':
           displayContact();
           break;
         case 'login':
           displayLogin();
           break;
+        case 'logout':
+          performLogout();
         case 'register':
-          displayRegister();
           break;
         case 'contact-list':
           displayContactList();
